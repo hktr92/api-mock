0b4e87089d93b3034cca989d6ada29f4eb240e83
Petru Szemereczki <petru.szemereczki@tazz.ro>
2021-10-18 12:34:37 +0300

    - initial work on api-mock

---
+++ service.ts
@@ -0,0 +1,29 @@
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+
+const _respond = (data: any, status: boolean = true): any => {
+    return {
+        status,
+        data
+    }
+}
+
+const router = new Router();
+router.get("/", (ctx: any) => {
+    ctx.response.body = _respond({ message: 'hello, friend!'});
+});
+router.post("/auth/login", (ctx: any) => {
+    ctx.response.body = _respond({ message: 'hello, friend!'});
+});
+router.post("/auth/logout", (ctx: any) => {
+    ctx.response.body = _respond({ message: 'hello, friend!'});
+});
+
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.addEventListener(
+    "listen",
+    (e: any) => console.log("Listening on http://localhost:8080"),
+);
+await app.listen({ port: 8080 });
