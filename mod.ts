1eac00a51f81da248866e89b505c4fea2645d319
Petru Szemereczki <petru.szemereczki@tazz.ro>
2021-10-18 12:36:35 +0300

    - better code structure for deno

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router } from "./deps.ts"
 
 const _respond = (data: any, status: boolean = true): any => {
     return {
