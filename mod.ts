ff2fc98fb18e6929192f40d008556a5e25700225
Petru Szemereczki <petru.szemereczki@tazz.ro>
2021-10-18 12:42:33 +0300

    - added dummy user on /auth/login

--- mod.ts
+++ mod.ts
@@ -1,21 +1,25 @@
-import { Application, Router } from "./deps.ts"
+import { Application, Router, v4 } from "./deps.ts";
 
 const _respond = (data: any, status: boolean = true): any => {
-    return {
-        status,
-        data
-    }
-}
+  return {
+    status,
+    data,
+  };
+};
 
 const router = new Router();
 router.get("/", (ctx: any) => {
-    ctx.response.body = _respond({ message: 'hello, friend!'});
+  ctx.response.body = _respond({ message: "hello, friend!" });
 });
 router.post("/auth/login", (ctx: any) => {
-    ctx.response.body = _respond({ message: 'hello, friend!'});
+  ctx.response.body = _respond({
+    token: crypto.randomUUID(),
+    name: "Katheryn Birkhoff",
+    email: "katheryn.birkhoff@example.com",
+  });
 });
 router.post("/auth/logout", (ctx: any) => {
-    ctx.response.body = _respond({ message: 'hello, friend!'});
+  ctx.response.body = _respond({ message: "hello, friend!" });
 });
 
 const app = new Application();
@@ -23,7 +27,7 @@ app.use(router.routes());
 app.use(router.allowedMethods());
 
 app.addEventListener(
-    "listen",
-    (e: any) => console.log("Listening on http://localhost:8080"),
+  "listen",
+  (e: any) => console.log("Listening on http://localhost:8080"),
 );
 await app.listen({ port: 8080 });
